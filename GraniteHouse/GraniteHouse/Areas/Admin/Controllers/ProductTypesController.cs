195e34c74085d44c5aadd4c11aa10eeda93f71e8
addna <addna@DESKTOP-MFNARD1>
2019-08-05 16:59:33 +0200

    Product Type- Delete Action Method

--- GraniteHouse/GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -95,5 +95,34 @@ namespace GraniteHouse.Areas.Admin.Controllers
 
             return View(productType);
         }
+
+
+        //GET Delete action Method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Delete action Method
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));        }
     }
 }
