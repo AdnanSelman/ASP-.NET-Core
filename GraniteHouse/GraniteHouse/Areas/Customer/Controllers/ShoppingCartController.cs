551503e7cf21c6b09edd3c3d52c4f3d179a15aa7
addna <addna@DESKTOP-MFNARD1>
2019-08-09 18:19:01 +0200

    ShoppingCart Post Action Method

--- GraniteHouse/GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
+++ GraniteHouse/GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -42,5 +42,39 @@ namespace GraniteHouse.Areas.Customer.Controllers
             }
             return View(ShoppingCartVM);
         }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            List<int> lstCartItem = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+
+            ShoppingCartVM.Appointments.AppointmentDate = ShoppingCartVM.Appointments.AppointmentDate
+                                                           .AddHours(ShoppingCartVM.Appointments.AppointmentTime.Hour)
+                                                           .AddMinutes(ShoppingCartVM.Appointments.AppointmentTime.Minute);
+
+            Appointments appointments = ShoppingCartVM.Appointments;
+            _db.Appointments.Add(appointments);
+            _db.SaveChanges();
+
+            int appointmentId = appointments.Id;
+
+            foreach(int productId in lstCartItem)
+            {
+                ProductsSelectedForAppointment productsSelectedForAppointment = new ProductsSelectedForAppointment()
+                {
+                    AppointmentId = appointmentId,
+                    ProductId = productId
+                };
+                _db.ProductsSelectedForAppointment.Add(productsSelectedForAppointment);
+            }
+            _db.SaveChanges();
+            lstCartItem = new List<int>();
+            HttpContext.Session.Set("ssShoppingCart", lstCartItem);
+
+            return RedirectToAction("Index");
+        }
     }
 }
